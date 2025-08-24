8a135efec6194e6f8917732ecfc90b6201d348ce
tadosuke <tadosuke@gmail.com>
2025-08-24 15:25:11 +0900

    refactor: clean up snake body implementation

--- src/entities/Snake.ts
+++ src/entities/Snake.ts
@@ -1,24 +1,24 @@
 import { type Point } from '../types/types';
 
-const INITIAL_BODY_LENGTH = 3;
-
 export class Snake {
-  private head: Point;
   private body: Point[];
   private direction: string;
 
   constructor(startX: number, startY: number) {
-    this.head = { x: startX, y: startY };
-    this.body = [
+    this.body = this.createInitialBody(startX, startY);
+    this.direction = 'right';
+  }
+
+  private createInitialBody(startX: number, startY: number): Point[] {
+    return [
       { x: startX, y: startY },     // Head
       { x: startX - 1, y: startY }, // Body segment behind head  
       { x: startX - 2, y: startY }  // Tail segment
     ];
-    this.direction = 'right';
   }
 
   getHead(): Point {
-    return this.head;
+    return this.body[0];
   }
 
   getBody(): Point[] {
